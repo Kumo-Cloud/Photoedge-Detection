508cb720c13ada965dc7a0f0221570b6d28f158e
Cloud <cloud735457447@gmail.com>
2017-03-10 19:01:32 +0800

    transport block size

--- Math.m
+++ Math.m
@@ -2,13 +2,13 @@
 % It can test each ten shape of blocks
 % type by @Kumo-Cloud@ email me when you have question
 
-function [tpye] = Math(IC)
+function [tpye] = Math(IC,num)
 
 math_percentage = zeros(1,10);
 x = 1;
 for order = 1:10
     Simple_b = Information(order);
-    math_percentage(x) = Try(IC, Simple_b);
+    math_percentage(x) = Try(IC, Simple_b, num);
     x = x + 1;
 end
 
