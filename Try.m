1c49cd332eb847e0de8e241977c63a1db9c5b7d9
Cloud <cloud735457447@gmail.com>
2017-03-10 19:00:51 +0800

    fix auto-size

--- Try.m
+++ Try.m
@@ -2,10 +2,10 @@
 % It can test the block
 % type by @Kumo-Cloud@ email me when you have question
 
-function [percentage] = Try(IC, Simple_b)
+function [percentage] = Try(IC, Simple_b, num)
 
 percentage = 0;
-for i = 1:16
+for i = 1:num
     x = IC(i)*Simple_b(i);
     percentage = x + percentage;
 end
